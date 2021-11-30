30942b07c29758e3da687173095d8bff051bf23d
AnHoangbk19 <an.phambk19@hcmut.edu.vn>
2021-11-30 20:24:50 +0700

    fixed

--- mvc/views/ManageEmp.php
+++ mvc/views/ManageEmp.php
@@ -14,7 +14,7 @@ $home_url = getUrl().'/';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/app.css">
+    <link rel="stylesheet" href="./assets/css/appA.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Document</title>
@@ -33,7 +33,7 @@ $home_url = getUrl().'/';
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto fontSize">
                     <li class="nav-item active mr-4 selectedMenu">
-                        <a class="nav-link" href="./home">Back To Home<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="./index/viewAll">Back To Home<span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </div>
